65ef10c0e2593b21b0322908f936989deb4da369
sofih-ii <sofiasaramoratrujillo@gmail.com>
2024-04-04 14:25:48 -0500

    Null, Undefined, Symboll y BigInt

---
+++ tiposdedatosprimitivos_null_undefined_symbol_bigint.js
@@ -0,0 +1,27 @@
+//Null
+const ventanas = null
+console.log(ventanas)
+console.log(typeof ventanas)
+
+//Undefined
+let name
+console.log(name)
+
+//Symbol
+const uniqueId = Symbol('id')
+const symbol1 = Symbol(1)
+const symbol2 = Symbol(1)
+console.log(symbol1 === symbol2)
+
+const iD = Symbol('id')
+let user = {}
+user[iD] = '12345546'
+console.log(user)
+
+//Bigint
+const bigNumber = 4527365726525267477n
+console.log(bigNumber)
+
+const numberOfparticlesInTheUniverse = 10000000000000000000000000000000000n
+console.log(numberOfparticlesInTheUniverse)
+
