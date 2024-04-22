f9eab7705cd901ef0ecaf2fe5d55cce2289aa883
sofih-ii <sofiasaramoratrujillo@gmail.com>
2024-04-22 14:21:41 -0500

    Example and exercises on comparison operators

---
+++ comparison_operators.js
@@ -0,0 +1,26 @@
+//Operadores de comparacion
+/*
+"==" -> Igualdad : verifica si los valores son verdad y devuelve un true.
+"===" -> Igualdad estrictiva : verifica si los valores son iguales y del mismo tipo y devuelve true.
+"!=" -> Diferente : Verifica si un valor es diferente que el otro.
+"!==" -> Diferente estrictivo: verifica si un valor es diferente que otro y tambien su tipo de dato.
+">" -> Mayor que : verifica si el primer dato es mayor que otro dato.
+"<" -> Menor que : Verifica si el primer dato es enor que el otro dato.
+">=" -> Mayor o igual que: verifica que el pirmer dato es o mayor o igual que el siguiente dato proporcionado.
+"<=" -> Menor o igual que: Verifica si el primer dato es menor o igual que el sigunete dato proporcioando.
+*/
+const a = 10
+const b = 20
+const c = "10"
+
+console.log(a == c)
+console.log(a === c)
+console.log(a == b)
+console.log(a != c)
+console.log(a != b)
+console.log(a !== b)
+console.log(a !== c)
+console.log(a > c)
+console.log(a < b)
+console.log(a >= c)
+console.log(a <= b)
