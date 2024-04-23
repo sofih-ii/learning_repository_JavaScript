8187858e8babf087fb0a406bc03cc28bbe808fbf
sofih-ii <sofiasaramoratrujillo@gmail.com>
2024-04-23 16:39:12 -0500

    example of conditional ternary

---
+++ conditional_ternary.js
@@ -0,0 +1,18 @@
+/*let message 
+
+const age = 20
+
+if(age >= 18){
+    message = "Is older of age"
+}else{
+    message = "No, he is not  older of legal age"
+}
+
+console.log(message)*/
+// conditional ? true : false
+
+const age = 10
+
+const message = age >= 18 ? "Is older of age" : "No, he is not  older of legal age"
+
+console.log(message)
