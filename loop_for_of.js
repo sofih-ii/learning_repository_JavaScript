f27f55f123ea3526eec47721e1f6c31bfbc3104e
sofih-ii <sofiasaramoratrujillo@gmail.com>
2024-04-24 12:02:06 -0500

    Adding syntax of Forof

---
+++ loop_for_of.js
@@ -0,0 +1,13 @@
+/*
+loop for of: iterables objects whit -> array, strings []
+
+for(value of object){
+    code execute 
+}
+*/
+
+let basket = ["apple", "orange", "pear", "grape"]
+
+for(element of basket){
+    console.log(element)
+}
