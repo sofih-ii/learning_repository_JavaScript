10bc82f7265fd5ccee542215f58502b4773f413f
sofih-ii <sofiasaramoratrujillo@gmail.com>
2024-04-24 14:42:22 -0500

    Added file above loop while

---
+++ loop_while.js
@@ -0,0 +1,16 @@
+/*
+while(conditional){
+    code execute
+}
+*/
+let contador = 0
+while(contador <= 10){
+    console.log(contador)
+    contador++
+}
+console.log(" ")
+let contador1 = 0
+while(contador1 <= 10){
+    contador1++
+    console.log(contador1)
+}
