cbf341e13daa87e77c5e74155ce3dbe838d19087
sofih-ii <sofiasaramoratrujillo@gmail.com>
2024-04-26 22:39:14 -0500

    Example of methods that modify the original arrays: push() and pop()

---
+++ modifiers_push_pop.js
@@ -0,0 +1,16 @@
+/*
+Methods that modify the original arrays
+*/
+
+//push()
+
+const fruit = Array('Apple', 'Orange', 'Pear', 'Grape')
+console.log(fruit)
+
+fruit.push('pineapple', 'watermelon')
+console.log(fruit)
+
+//pop()
+fruit.pop()
+console.log(fruit)
+
