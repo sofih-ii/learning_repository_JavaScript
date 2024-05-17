f987420731b6d98d12fd2902717c948d56526fa6
sofih-ii <sofiasaramoratrujillo@gmail.com>
2024-05-17 12:04:28 -0500

    Prototype in practice

--- classes_and_objects/inheritance_and_prototypes.js
+++ classes_and_objects/inheritance_and_prototypes.js
@@ -25,3 +25,23 @@ const dog1 = new Dog("Boby", "Dog", "Golden retriever")
 console.log(dog1);
 dog1.run();
 dog1.sound();
+
+dog1.newInheritance = function(){
+    console.log("This is a method");
+}
+
+dog1.newInheritance()
+
+Dog.prototype.secondMethod = function(){
+    console.log("It's another new method");
+}
+console.log(Dog.prototype)
+
+console.log(Dog.prototype.secondMethod())
+
+let currentPrototype = Object.getPrototypeOf(dog1);
+
+while (currentPrototype !== null) {
+    console.log(currentPrototype);
+    currentPrototype = Object.getPrototypeOf(currentPrototype);
+}
